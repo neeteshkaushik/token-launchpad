9da466c714ceb4c2e5ec26c0318ffe701109472d
neeteshkaushik <36589237+neeteshkaushik@users.noreply.github.com>
2024-10-02 15:03:02 +0530

    fixed build fail error

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import Header from "./components/Header";
 import TokenLaunchPad from "./components/TokenLaunchPad";
-import { CustomWalletProvider } from "./contexts/WalletContext";
 import MyTokens from "./components/MyTokens";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -16,7 +15,6 @@ function App() {
     <ConnectionProvider endpoint="https://api.devnet.solana.com">
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <CustomWalletProvider>
             <Router>
               <Header />
               <Routes>
@@ -24,7 +22,6 @@ function App() {
                 <Route path="/myTokens" element={<MyTokens />} />
               </Routes>
             </Router>
-          </CustomWalletProvider>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
